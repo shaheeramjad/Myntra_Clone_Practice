6c039482ce8cd84de45a863c2c7ebec30561bb78
Shaheer Amjad <155566482+shaheeramjad@users.noreply.github.com>
2024-08-18 16:14:30 +0500

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,45 @@
+import { IoMdPerson } from "react-icons/io";
+import { FaShieldHeart } from "react-icons/fa6";
+import { BsBagHeartFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+  <header>
+  <div className="logo_container">
+      <Link to="/"><img className="myntra_home" src="images/myntra_logo.webp" alt="Myntra Home"/></Link>
+  </div>
+  <nav className="nav_bar">
+      <a href="#">Men</a>
+      <a href="#">Women</a>
+      <a href="#">Kids</a>
+      <a href="#">Home & Living</a>
+      <a href="#">Beauty</a>
+      <a href="#">Studio <sup>New</sup></a>
+  </nav>
+  <div className="search_bar">
+      <span className="material-symbols-outlined search_icon">search</span>
+      <input className="search_input" placeholder="Search for products, brands and more"/>
+  </div>
+  <div className="action_bar">
+      <div className="action_container">
+      <IoMdPerson />
+          <span className="action_name">Profile</span>
+      </div>
+
+      <div className="action_container">
+<FaShieldHeart />
+          <span className="action_name">Wishlist</span>
+      </div>
+
+      <Link className="action_container" to="/bag">
+<BsBagHeartFill />
+          <span className="action_name">Bag</span>
+          <span className="bag-item-count">0</span>
+      </Link>
+  </div>
+</header>
+  )
+}
+
+export default Header;
